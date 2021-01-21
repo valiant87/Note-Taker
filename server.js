47bc10148d3dcc6ebc60fc40b19a000160305f77
Val Nimirenco <valnimirenco@gmail.com>
2021-01-20 19:01:23 -0800

    fixed var to const

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 
-var express = require("express");
+const express = require("express");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -11,10 +11,10 @@ var express = require("express");
 // ==============================================================================
 
 // Tells node that we are creating an "express" server
-var app = express();
+const app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
